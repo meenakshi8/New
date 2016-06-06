e4c43e0b6fc8a508024d26165c51748c3640ddc8
meenakshi8 <meenakshik.kathayat4@gmail.com>
2016-06-06 14:25:53 -0700

    First Commit

---
+++ src/mm/m.java
@@ -0,0 +1,17 @@
+package mm;
+
+public class m {
+public int a;
+public m(int b){
+	this.a=b;
+}
+/*public void msg()
+{
+	if(false)
+	{
+		System.out.println("abc");
+		System.out.println("abc");
+		System.out.println("abc");
+	}
+}*/
+}
---
+++ src/nn/n.java
@@ -0,0 +1,23 @@
+package nn;
+import mm.m;
+/*import java.util.Scanner;*/
+
+public class n {
+	
+	public static void main(String args[])
+	{
+		int b=5;
+		/*Scanner s=new Scanner(System.in);
+		b = s.nextInt();*/
+		if(b<4&&6<b)
+		{
+			System.out.println("h");
+		}
+		
+		m m1=new m(4);
+	System.out.println(m1.a);
+	}
+	
+}
+
+
